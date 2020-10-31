187eafce4fbe2eb00b8ecf393d634355ba04b4f5
hungabc <70497683+hungabc@users.noreply.github.com>
2020-10-31 08:22:04 +0700

    31/10/2020

--- src/app/donhang/donhang.component.ts
+++ src/app/donhang/donhang.component.ts
@@ -7,14 +7,32 @@ import { BaseComponent } from '../lib/base-component';
   styleUrls: ['./donhang.component.css']
 })
 export class DonhangComponent extends BaseComponent implements OnInit {
-donhangs:any;
+tg:any;
+  donhangs:any;
+index:any;
+size: any;
+trangthai:any;
+total:any;
+dstt:any;
   constructor(injector:Injector) {
     super(injector);
   }
 
   ngOnInit(): void {
-    this._api.get('/api/Donhang/get-all').takeUntil(this.unsubscribe).subscribe(res => {
-      this.donhangs = res;
+    this.dstt=[
+      {name:"chưa xác nhận"},
+      {name:"đã xác nhận"},
+      {name:"đã đóng gói"},
+      {name:"đang giao hàng"},
+      {name:"đã nhận hàng"},
+      {name:"đã thanh toán"},
+      {name:"đã hủy đơn"}
+    ]
+    this.index=1,this.size=12;this.total=0;
+    this._api.get('/api/Donhang/get-all/'+this.index+'/'+this.size).takeUntil(this.unsubscribe).subscribe(res => {
+      this.tg = res;
+      this.donhangs=this.tg.data;
+      this.total=this.tg.totalItems;
       setTimeout(() => {
         this.loadScripts();
       });
@@ -22,11 +40,35 @@ donhangs:any;
    
   }
   reload(){
-    this._api.get('/api/Donhang/get-all').takeUntil(this.unsubscribe).subscribe(res => {
-      this.donhangs = res;
+    this.index=1,this.size=12;this.total=0;
+    this._api.get('/api/Donhang/get-all/'+this.index+'/'+this.size).takeUntil(this.unsubscribe).subscribe(res => {
+      this.tg = res;
+      this.donhangs=this.tg.data;
+      this.total=this.tg.totalItems;
     
     }); 
    
+  }
+  loadPage(page){
+    this.index=1,this.size=12;this.total=0;
+    this._api.get('/api/Donhang/get-all/'+page+'/'+this.size).takeUntil(this.unsubscribe).subscribe(res => {
+      this.tg = res;
+      this.donhangs=this.tg.data;
+      this.total=this.tg.totalItems;
+    
+    }); 
+  }
+  getbytt(trangthai){
+    this.index=1;this.size=1000;this.total=0;
+    if(trangthai=='all') this.reload();
+    else {
+      this._api.get('/api/Donhang/get-all/'+this.index+'/'+this.size+'/'+trangthai).takeUntil(this.unsubscribe).subscribe(res => {
+        this.tg = res;
+        this.donhangs=this.tg.data;
+        this.total=this.tg.totalItems;
+      
+      }); 
+    }
   }
   thaydoitrangthai(masp){
     if(confirm('bạn có chắc không?')){
